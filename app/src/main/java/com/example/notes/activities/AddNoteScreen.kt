ebec85c5779aa5681a4a575d68e32a282d1ecfa7
fnava01 <fernando.navarrete@toka.mx>
2024-02-12 17:57:08 -0600

    added add notes screen

--- app/src/main/java/com/example/notes/activities/AddNoteScreen.kt
+++ app/src/main/java/com/example/notes/activities/AddNoteScreen.kt
@@ -2,11 +2,45 @@ package com.example.notes.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.notes.R
+import com.google.android.material.textfield.TextInputEditText
 
 class AddNoteScreen : AppCompatActivity() {
+    
+    private lateinit var titleText : TextView
+    private lateinit var noteTitleEditText : TextInputEditText
+    private lateinit var noteBodyTitleEditText: TextInputEditText
+    private lateinit var saveButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note_screen)
+        
+        titleText = findViewById(R.id.add_note_screen_title)
+        noteTitleEditText = findViewById(R.id.note_title_text_field)
+        noteBodyTitleEditText = findViewById(R.id.note_body_text_field)
+        saveButton = findViewById(R.id.save_note_button)
+        
+        saveButton.setOnClickListener {
+            if(validateForm()) {
+                saveNote()
+            }
+            else {
+                Toast.makeText(this, "Por favor escriba algo!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        
+    }
+    
+    private fun validateForm() : Boolean {
+        if(noteTitleEditText.text.isNullOrEmpty() || noteBodyTitleEditText.text.isNullOrEmpty()) {
+            return false
+        }
+        return true
+    }    
+    private fun saveNote() {
+
     }
 }
--- app/src/main/java/com/example/notes/models/Note.kt
+++ app/src/main/java/com/example/notes/models/Note.kt
@@ -1,7 +1,8 @@
 package com.example.notes.models
 
+import java.io.Serializable
 import java.util.Date
 
-class Note(var content: String, var title : String, var date : Date) {
+class Note(var content: String, var title : String, var date : Date) : Serializable {
 
 }
